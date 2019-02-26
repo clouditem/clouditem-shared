352d4df73911834c934ed46fe9dddc81f80009ed
Jakob Löw <jal@agn.email>
2019-02-26 14:37:20 +0100

    fix syntax errors in db_util.js

--- src/db_util.js
+++ src/db_util.js
@@ -11,13 +11,13 @@ module.exports = db => (table, ...var_names) => {
     insert_clause = req => `INSERT INTO ${table} (${filtered_parameter(req)
       .join(',')}) VALUES (${filtered_parameter(req)
       .map(str => `$\{${str}}`)
-      .join(',')}) RETURNING ${var_list}`;
+      .join(',')})`;
   return {
     select_all: () => db.any(`SELECT ${var_list}  FROM ${table}`),
     select_where: req => db.any(`SELECT ${var_list} FROM ${table} WHERE ${filtered_where_clause(req)}`, req.promised_params),
-    insert_into: req => db.one(insert_clause(req), req.promised_params),
-    insert_or_update: (req, identifier) => db.one(`${insert_clause(req)}
-       ON CONFLICT (${identifier}) DO SET ${filtered_parameter(req)
+    insert_into: req => db.one(`${insert_clause(req)} RETURNING ${var_list}`, req.promised_params),
+    insert_or_update: (req, identifier) => db.none(`${insert_clause(req)
+    } ON CONFLICT (${identifier}) DO UPDATE SET ${filtered_parameter(req)
       .filter(str => str !== identifier)
       .map(str => `${str} = $\{${str}}`)
       .join(',')}`, req.promised_params),
