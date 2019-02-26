7efc3b1d414e7ec850f90f37f52aee756b00be4a
sin <simon@neumair.email>
2019-02-26 12:57:56 +0100

    adjusted env default calculation to allow falsy values

--- src/env.js
+++ src/env.js
@@ -8,8 +8,10 @@ module.exports = properties => {
 
     if (typeof properties[key] === 'function')
       env[key] = properties[key](process.env[key]);
-    else if (process.env[key] || properties[key])
-      env[key] = process.env[key] || properties[key];
+    else if (typeof process.env[key] !== undefined)
+      env[key] = process.env[key];
+    else if (typeof properties[key] !== undefined) // eslint-disable-line no-negated-condition
+      env[key] = properties[key];
     else
       throw Error(`environment property ${key} was not found and has no default`);
 
