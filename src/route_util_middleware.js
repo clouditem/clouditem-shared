a7ee132bf13873de93cd35c525d7a690c3b2b365
sin <simon@neumair.email>
2019-03-26 10:45:52 +0100

    moved jwt to route_util_middleware, version 1.5.1

--- src/route_util_middleware.js
+++ src/route_util_middleware.js
@@ -5,11 +5,44 @@ const {promise_value} = require('./util'),
     TypeError: (res, err) => res.status(400).send(`Bad Request\n${err}`),
     default: (res, err) => (console.dir(err), res.status(500).send(`Internal Server Error\n${err}`)),
   },
-  bad_request = res => err => (error_handlers[err.name] || error_handlers.default)(res, err);
+  bad_request = res => err => (error_handlers[err.name] || error_handlers.default)(res, err),
+  express_jwt = require('express-jwt'),
+  fetch = require('node-fetch'),
+  cached_jwt_secret_fetch = (url, period) => {
 
+    let cache = '',
+      next_fetch = 0;
+
+    return (req, payload, done) => {
+
+      if (Date.now() < next_fetch)
+        return done(null, cache);
+
+      next_fetch = Date.now() + 60 * 1000 * period;
+      return fetch(url)
+        .then(res => res.text())
+        .then(text => cache = text)
+        .then(text => done(null, text));
+
+    };
+
+  };
 
 module.exports = {
   cors,
+  jwt: ({jwt_secret_url, jwt_secret_refresh_period, require_credentials = false}) => {
+
+    if (!jwt_secret_url)
+      throw new Error('Missing property jwt_secret_url');
+    if (!jwt_secret_refresh_period)
+      throw new Error('Missing property jwt_secret_refresh_period');
+
+    return express_jwt({
+      secret: cached_jwt_secret_fetch(jwt_secret_url, jwt_secret_refresh_period),
+      credentialsRequired: require_credentials,
+    });
+
+  },
   error_handlers: obj => (Object.assign(error_handlers, obj), (req, res, next) => next()),
   json: (limit = '20mb') => body_parser.json({limit}),
   require_permissions: string_or_array_of_perms => (req, res, next) => {
@@ -37,7 +70,7 @@ module.exports = {
 
     if (permissions.indexOf('creator') !== -1
       && !user.is_creator)
-      return bad_request(res)(new TypeError('You do not have the \'write\' permission'));
+      return bad_request(res)(new TypeError('You do not have the \'creator\' permission'));
 
     return next();
 
@@ -51,6 +84,12 @@ module.exports = {
     return res.redirect('/login');
 
   },
+  fixed_parameters: obj => (req, res, next) => {
+
+    req.promised_params = {...obj, ...req.promised_params || {}};
+    next();
+
+  },
   request_parameters: string_or_array_of_params => (req, res, next) => {
 
     const params = Array.isArray(string_or_array_of_params)
@@ -58,7 +97,8 @@ module.exports = {
       : [string_or_array_of_params];
 
     return Promise.all(
-      params.map(param => promise_value(param).in(['params', 'body', 'query']).of_request(req))
+      params.map(param => promise_value(param).in(['params', 'body', 'query'])
+        .of_request(req))
     )
       .then(() => next())
       .catch(bad_request(res));
